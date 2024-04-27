0e546332b2b25d5f3ab4b410b26a60341022c4a6
letittya <letitia.anda@gmail.com>
2024-04-27 15:58:47 +0300

    added functions n lcd messages

--- smart_lock/read.py
+++ smart_lock/read.py
@@ -5,51 +5,62 @@ from rpi_lcd import LCD
 
 lcd=LCD()
 
-lcd.text("hello ebi",1)
-
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
 buzzer = 35
 GPIO.setup(buzzer,GPIO.OUT)
 
-relay_module = 37
-GPIO.setup(relay_module,GPIO.OUT)
+#relay_module = 37
+#GPIO.setup(relay_module,GPIO.OUT)
 
 good_id = "789061940596"
 
 rfid_tag=SimpleMFRC522()
 
-while True:
-	print("Place your tag to scan")
-	id, data = rfid_tag.read()
-	print(id)
-	print(data)
-	id=str(id)
-	
+def compare_ids_successful(id,good_id):
 	if(id==good_id):
 		print("Permission granted")
-		GPIO.output(relay_module,GPIO.HIGH)
+		lcd.clear()
+		lcd.text("Permission",1)
+		lcd.text("granted",2)
+		#GPIO.output(relay_module,GPIO.HIGH)
 		GPIO.output(buzzer,GPIO.HIGH)
 		time.sleep(1)
 		GPIO.output(buzzer,GPIO.LOW)
 		time.sleep(5)
-		GPIO.output(relay_module,GPIO.LOW)
+		#GPIO.output(relay_module,GPIO.LOW)
 		
-	else:
+def not_permitted_buzzer():
+	GPIO.output(buzzer,GPIO.HIGH)
+	time.sleep(0.5)
+	GPIO.output(buzzer,GPIO.LOW)
+	time.sleep(0.5)
+		
+def compare_ids_NOT_successful(id,good_id):
+	if(id!=good_id):
 		print("Permission denied")
-		GPIO.output(buzzer,GPIO.HIGH)
-		time.sleep(0.5)
-		GPIO.output(buzzer,GPIO.LOW)
-		time.sleep(0.5)
-		GPIO.output(buzzer,GPIO.HIGH)
-		time.sleep(0.5)
-		GPIO.output(buzzer,GPIO.LOW)
-		time.sleep(0.5)
-		GPIO.output(buzzer,GPIO.HIGH)
-		time.sleep(0.5)
-		GPIO.output(buzzer,GPIO.LOW)
+		lcd.clear()
+		lcd.text("Permission",1)
+		lcd.text("denied",2)
+		for i in range(3):
+			not_permitted_buzzer()
 		
+	
+
+while True:
+	# read and get the data+id from the rfid card 
+	print("Place your card to scan")
+	# now print the text to the lcd screen
+	lcd.text("Place your", 1)
+	lcd.text("card to scan", 2)
+	id, data = rfid_tag.read()
+	id=str(id) #converting from int -> string
+	print(id)
+	print(data)
+	
+	compare_ids_successful(id,good_id) 
+	compare_ids_NOT_successful(id,good_id)
 	
 
 
