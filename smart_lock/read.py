5cf445f9fb0ac94525206dfef867f4a33ae3334b
letittya <letitia.anda@gmail.com>
2024-04-29 14:31:08 +0300

    added bicolor led

--- smart_lock/read.py
+++ smart_lock/read.py
@@ -15,6 +15,11 @@ relay_module = 37
 GPIO.setup(relay_module,GPIO.OUT)
 GPIO.output(relay_module,GPIO.HIGH)
 
+green_led=13
+red_led=11
+GPIO.setup(green_led,GPIO.OUT)
+GPIO.setup(red_led,GPIO.OUT)
+
 good_id = "789061940596"
 
 rfid_tag=SimpleMFRC522()
@@ -25,17 +30,21 @@ def compare_ids_successful(id,good_id):
 		lcd.clear()
 		lcd.text("Permission",1)
 		lcd.text("granted",2)
+		GPIO.output(green_led,GPIO.HIGH)
 		GPIO.output(relay_module,GPIO.LOW)
 		GPIO.output(buzzer,GPIO.HIGH)
 		time.sleep(1)
 		GPIO.output(buzzer,GPIO.LOW)
 		time.sleep(5)
 		GPIO.output(relay_module,GPIO.HIGH)
+		GPIO.output(green_led,GPIO.LOW)
 		
 def not_permitted_buzzer():
 	GPIO.output(buzzer,GPIO.HIGH)
+	GPIO.output(red_led,GPIO.HIGH)
 	time.sleep(0.5)
 	GPIO.output(buzzer,GPIO.LOW)
+	GPIO.output(red_led,GPIO.LOW)
 	time.sleep(0.5)
 		
 def compare_ids_NOT_successful(id,good_id):
@@ -44,6 +53,7 @@ def compare_ids_NOT_successful(id,good_id):
 		lcd.clear()
 		lcd.text("Permission",1)
 		lcd.text("denied",2)
+		GPIO.output(red_led,GPIO.HIGH)
 		for i in range(3):
 			not_permitted_buzzer()
 		
