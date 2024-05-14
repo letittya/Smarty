cbc584549aa1e35c98ded0dce6212edab71396aa
Letitia <letitia.anda@gmail.com>
2024-05-14 23:49:46 +0300

    added imgs

--- SmartApp/constants/images.js
+++ SmartApp/constants/images.js
@@ -2,5 +2,7 @@ import logo from "../assets/images/logo.png";
 import empty from "../assets/images/empty.png";
 import lock_img from "../assets/images/lock_img.png";
 import temp_img from "../assets/images/temp_img.png";
+import blinds_img from "../assets/images/blinds_img.png";
+import led_img from "../assets/images/led_img.png";
 
-export default { temp_img , logo, empty, lock_img };
+export default { led_img,temp_img , logo, empty, lock_img, blinds_img };
